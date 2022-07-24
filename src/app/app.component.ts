70b9e8a4ecf9ac05d6a6746e8173674d237aaad1
alucard-dev <moeseenkoa@gmail.com>
2022-07-24 16:51:35 +0300

    small fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,6 @@ export class AppComponent implements OnInit, OnDestroy {
 
   initCharacterEvents(): void {
     // 1.2. Add API call on each user input. Use mockDataService.getCharacters - to make get request.
-    // this.searchTermByCharacters.subscribe(x=>this.mockDataService.getCharacters(x))
     // 2. Since we don't want to spam our service add filter by input value and do not call API until a user enters at least 3 chars.
     // 3. Add debounce to prevent API calls until user stop typing.
 
